3c97b9a2a1468103a31389d182adacb115a11643
AleksandarKaraganchev <aleksandar.karaganchev546@edu1.pgzaimov.com>
2023-05-02 20:36:57 +0300

    First commit

---
+++ BroiNaRealniteChisla/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroiNaRealniteChisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+            foreach (var num in numbers)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (var item in counts.Keys)
+            {
+                Console.WriteLine($"{item} -> {counts[item]}");
+            }
+        }
+    }
+}
---
+++ PodobrenTelefonenUkazatel/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodobrenTelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            var sortedPhonebook = new SortedDictionary<string, string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                string[] separator = input.Split();
+                if (separator[0] == "A")
+                {
+                    string name = separator[1];
+                    string phoneNum = separator[2];
+
+                    phonebook[name] = phoneNum;
+                    sortedPhonebook[name] = phoneNum;
+                }
+                if (separator[0] == "S")
+                {
+                    string name = separator[1];
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                if (separator[0] == "ListAll")
+                {
+                    Console.WriteLine($"В указателя има {sortedPhonebook.Count} записани.");
+                    Console.WriteLine("Списък по азбучен ред:");
+                    foreach (var contact in sortedPhonebook)
+                    {
+                        Console.WriteLine($"{contact.Key} -> {contact.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
